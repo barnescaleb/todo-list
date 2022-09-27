119ee46547467121b2f708b9aaf9e60b3d5295e2
barnescaleb <barnescaleb119@gmail.com>
2022-09-27 10:54:18 -0700

    add today module

--- src/today.js
+++ src/today.js
@@ -1,4 +1,12 @@
 export function today() {
 
+    const contentDiv = document.querySelector(".content")
 
+    // clears existing DOM elements
+    contentDiv.replaceChildren();
+
+    const header = document.createElement("h1");
+    header.textContent = "Today";
+    header.classList.add("today");
+    contentDiv.appendChild(header);
 }
